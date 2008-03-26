0efc71e4e695dde34e4f5622ea4553d87c251c09
Urs Reupke <ursreupke@gmail.com>
2008-03-26 16:42:52 +0000

    Main Method more Java-Like appears to be important

--- Charm_Cards/src/net/sf/anathema/charm/cards/CardCreator.groovy
+++ Charm_Cards/src/net/sf/anathema/charm/cards/CardCreator.groovy
@@ -3,7 +3,7 @@ package net.sf.anathema.charm.cards
 import net.sf.anathema.character.generic.magic.ICharm
 class CardCreator {
 	
-  static void main(args) {
+  static void main(String[] args) {
 		ICharm[] charms = new CharmReader().read(new File(args[0]))
 		Writer writer = args.size() > 1 ? new FileWriter(new File(args[1])) : new StringWriter() 
 		new CardsWriter(writer).write(charms)
