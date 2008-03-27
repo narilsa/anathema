e76625bd7638bcfea3fc3be3aebfe352902bdceb
Urs Reupke <ursreupke@gmail.com>
2008-03-27 19:44:06 +0000

    Samples moved to Tests

--- Charm_Cards/test/net/sf/anathema/charm/cards/CardsWriterTest.groovy
+++ Charm_Cards/test/net/sf/anathema/charm/cards/CardsWriterTest.groovy
@@ -8,19 +8,34 @@ import org.custommonkey.xmlunit.XMLUnit
 import org.custommonkey.xmlunit.Diffimport net.sf.anathema.character.generic.magic.ICharmimport org.custommonkey.xmlunit.DifferenceListener
 import net.sf.anathema.character.generic.impl.backgrounds.CharacterTypeBackgroundTemplateimport org.custommonkey.xmlunit.ElementNameQualifier
 class CardsWriterTest extends GroovyTestCase {
+  static def EMPTYLIST = '''
+    <cards/>
+  '''
   
+  static def SINGLECARD= '''
+    <cards>
+      <card />
+    </cards>
+  '''
+  
+  static def MULTICARD= '''
+    <cards>
+      <card />
+      <card />
+    </cards>
+  '''
     void setUp() {
 	  XMLUnit.setIgnoreWhitespace(true)
     }
   
 	void testWritesEmptyCardsList() {
-	    def xmlDiff = new Diff(new CardsWriter().write(null), CardXmlSamples.EMPTYLIST)
+	    def xmlDiff = new Diff(new CardsWriter().write(null), EMPTYLIST)
 	    assert xmlDiff.similar()
 	}
 	
 	void testWritesSingleCardForSingleCharm(){
 	  String charmXml = new CardsWriter().write(createCharm("testId") )
-	  Diff diff = new Diff(charmXml, CardXmlSamples.SINGLECARD)
+	  Diff diff = new Diff(charmXml, SINGLECARD)
 	  DifferenceListener listener = new LayerDifferenceListener() 
 	  diff.overrideDifferenceListener(listener)
 	  assert diff.similar()
@@ -29,7 +44,7 @@ class CardsWriterTest extends GroovyTestCase {
 	void testMultipleCardsForMultipleObjects(){
 	  ICharm[] charms  = [createCharm("testId"), createCharm("toastId")]
 	  String charmXml = new CardsWriter().write(charms)
-	  Diff diff = new Diff(charmXml, CardXmlSamples.MULTICARD)
+	  Diff diff = new Diff(charmXml, MULTICARD)
 	  DifferenceListener listener = new LayerDifferenceListener() 
 	  diff.overrideDifferenceListener(listener)
 	  assert diff.similar()
--- Charm_Cards/test/net/sf/anathema/charm/cards/CardsWriter_NameTest.groovy
+++ Charm_Cards/test/net/sf/anathema/charm/cards/CardsWriter_NameTest.groovy
@@ -7,19 +7,33 @@ import org.custommonkey.xmlunit.XMLUnit
 import org.custommonkey.xmlunit.Diffimport net.sf.anathema.character.generic.magic.ICharmimport org.custommonkey.xmlunit.DifferenceListener
 import net.sf.anathema.character.generic.impl.backgrounds.CharacterTypeBackgroundTemplateimport org.custommonkey.xmlunit.ElementNameQualifier
 class CardsWriter_NameTest extends GroovyTestCase {
+  static def CARD_WITH_NAME= '''
+    <cards>
+      <card>
+        <name>testId</name>
+      </card>
+    </cards>
+  '''
   
+  static def CARD_WITH_OTHER_NAME= '''
+    <cards>
+      <card>
+        <name>toastId</name>
+      </card>
+    </cards>
+  '''
     void setUp() {
 	  XMLUnit.setIgnoreWhitespace(true)
     }
   
 	void testWritesNameElement(){
 	  String charmXml = new CardsWriter().write(createCharm("testId"))
-	  assertDifferenceLeniently(CardXmlSamples.CARD_WITH_NAME, charmXml)
+	  assertDifferenceLeniently(CARD_WITH_NAME, charmXml)
 	}
 	
 	void testFillsNameElementWithId(){
 	  String charmXml = new CardsWriter().write(createCharm("toastId"))
-	  assertDifferenceLeniently(CardXmlSamples.CARD_WITH_OTHER_NAME, charmXml)
+	  assertDifferenceLeniently(CARD_WITH_OTHER_NAME, charmXml)
 	}
 	
 	void assertDifferenceLeniently(controlXml, testXml){
