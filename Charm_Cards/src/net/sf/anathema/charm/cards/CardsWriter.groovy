c90b0cd7dc135e04e58182dcf6336759f83a9303
Urs Reupke <ursreupke@gmail.com>
2008-04-06 19:11:06 +0000

    CharmTypeStringBuilder to separate class

--- Charm_Cards/src/net/sf/anathema/charm/cards/CardsWriter.groovy
+++ Charm_Cards/src/net/sf/anathema/charm/cards/CardsWriter.groovy
@@ -1,14 +1,13 @@
 package net.sf.anathema.charm.cards
 
 import groovy.xml.*
-import net.sf.anathema.character.generic.impl.magic.charm.type.*
 import net.sf.anathema.character.generic.magic.*
-import net.sf.anathema.character.generic.magic.charms.type.*
 
 class CardsWriter {
 
 	Writer writer
 	Map pages = [:]
+	def builder = new CharmTypeStringBuilder()
 
 	def write(ICharm[] charms){
 		def xml = new MarkupBuilder(writer)
@@ -19,7 +18,7 @@ class CardsWriter {
 					exalt(charm.characterType)
 					ability(charm.primaryTraitType)
 					stats() {
-						type(buildTypeText(charm))
+						type(builder.build(charm))
 					}
 					keywords() {
 						charm.attributes.each {
@@ -41,43 +40,5 @@ class CardsWriter {
 
 	String createPageKey(String charmId, final String bookId){
 		return bookId + "." + charmId + ".Page";
-	}
-
-	String buildTypeText(ICharm charm){
-		def model = charm.charmTypeModel;
-		def type = model.charmType
-		StringBuilder builder = new StringBuilder(type.toString())
-		if (model.hasSpecialsModel()){
-			if (type == CharmType.Simple){
-				SimpleSpecialsModel specialsModel = model.specialsModel
-				builder.append(" (")
-				def speed = specialsModel.speed
-				def dv = specialsModel.defenseModifier
-				if (speed != 6){
-					builder.append("Speed ")
-					builder.append(specialsModel.speed)
-				}
-				if (dv != -1){
-					if (speed != 6){
-						builder.append(", ")
-					}
-					builder.append("DV ")
-					builder.append(dv)
-				}
-				builder.append(")")
-			}
-			if (type == CharmType.Reflexive){
-				ReflexiveSpecialsModel specialsModel = model.specialsModel
-				builder.append(" (Step ")
-				builder.append(specialsModel.primaryStep)
-				def secondaryStep = specialsModel.secondaryStep
-				if (secondaryStep){
-					builder.append(" or ")
-					builder.append(secondaryStep)
-				}
-				builder.append(")")
-			}
-		}
-		builder.toString()
 	}
 }
---
+++ Charm_Cards/src/net/sf/anathema/charm/cards/CharmTypeStringBuilder.groovy
@@ -0,0 +1,45 @@
+package net.sf.anathema.charm.cards
+
+import net.sf.anathema.character.generic.impl.magic.charm.type.*
+import net.sf.anathema.character.generic.magic.*
+
+class CharmTypeStringBuilder {
+
+	String build(ICharm charm){
+		def model = charm.charmTypeModel;
+		def type = model.charmType
+		StringBuilder builder = new StringBuilder(type.toString())
+		if (model.hasSpecialsModel()){
+			if (type == CharmType.Simple){
+				SimpleSpecialsModel specialsModel = model.specialsModel
+				builder.append(" (")
+				def speed = specialsModel.speed
+				def dv = specialsModel.defenseModifier
+				if (speed != 6){
+					builder.append("Speed ")
+					builder.append(specialsModel.speed)
+				}
+				if (dv != -1){
+					if (speed != 6){
+						builder.append(", ")
+					}
+					builder.append("DV ")
+					builder.append(dv)
+				}
+				builder.append(")")
+			}
+			if (type == CharmType.Reflexive){
+				ReflexiveSpecialsModel specialsModel = model.specialsModel
+				builder.append(" (Step ")
+				builder.append(specialsModel.primaryStep)
+				def secondaryStep = specialsModel.secondaryStep
+				if (secondaryStep){
+					builder.append(" or ")
+					builder.append(secondaryStep)
+				}
+				builder.append(")")
+			}
+		}
+		builder.toString()
+	}
+}
